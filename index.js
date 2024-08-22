5b426d187d237e856aa3ee93d45b53e38033590a
Joseph Camacho <spongkj@gmail.com>
2024-08-23 00:00:37 +0800

    original version node assignment1

--- index.js
+++ index.js
@@ -110,5 +110,63 @@
 
 // game();
 
+const { Command } = require("commander");
+const program = new Command();
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contacts.js");
 
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+// Function to invoke the appropriate action
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contacts = await listContacts();
+      console.log(contacts);
+      break;
+
+    case "get":
+      const contact = await getContactById(id);
+      if (contact) {
+        console.log(contact);
+      } else {
+        console.log(`Contact with id ${id} not found`);
+      }
+      break;
+
+    case "add":
+      if (name && email && phone) {
+        const newContact = await addContact(name, email, phone);
+        console.log("Added new contact:", newContact);
+      } else {
+        console.log("Please provide name, email, and phone to add a contact.");
+      }
+      break;
+
+    case "remove":
+      const updatedContacts = await removeContact(id);
+      console.log(`Removed contact with id ${id}. Updated contact list:`);
+      console.log(updatedContacts);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+// Invoke the action based on command-line input
+invokeAction(argv);
 
